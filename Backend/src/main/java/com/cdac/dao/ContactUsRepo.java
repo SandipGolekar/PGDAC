4917d9b16ca4318fff8b6ee029552c469e65c966
SandipGolekar <98867461+SandipGolekar@users.noreply.github.com>
2022-04-17 11:07:03 +0530

    Delete Backend directory

--- Backend/src/main/java/com/cdac/dao/ContactUsRepo.java
+++
@@ -1,9 +0,0 @@
-package com.cdac.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.cdac.pojos.ContactUs;
-
-public interface ContactUsRepo extends JpaRepository<ContactUs,Integer> {
-
-}
--- Backend/src/main/java/com/cdac/pojos/Roles.java
+++
@@ -1,6 +0,0 @@
-package com.cdac.pojos;
-
-public enum Roles {
-
-	PARENT, ADMIN
-}
